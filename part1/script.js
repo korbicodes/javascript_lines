60ee50896ae842b1f5ae00e134e66643baea1560
eKorbi <eralbakorbi22@gmail.com>
2025-09-16 19:14:15 +0200

    initial commit

---
+++ part1/script.js
@@ -0,0 +1,24 @@
+
+const javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+
+// typeof operator
+console.log(typeof true);   //boolean
+console.log(typeof 23); //number
+
+
+let year;
+console.log(year); //undefined (empty variable)
+console.log(typeof year); //undefined
+
+console.log(typeof null); //object - it's a bug , it should return null
+
+// when using const you need an initial value
+// by default alaways use const - less mutations is better
+// let is block scoped and var is function scoped
+
+
+//operator allows to transform values or combine multiple values
+
+ 
