2dfe53a0f548ffb94fb433e6e49956f09acce8fe
eKorbi <eralbakorbi22@gmail.com>
2025-09-20 14:16:27 +0200

    added one simple function

--- part2/script.js
+++ part2/script.js
@@ -216,3 +216,32 @@ console.log(employerOne.age);
 
 
 console.log(employerOne.getSummary());
+
+
+//arrays, looping
+//break completely terminate the whole loop, continue breaks in that current iteration and continue to the next one
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+const calcAvg = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+console.log(calcAvg([2, 3, 7]));
+console.log(calcAvg(totals));
+console.log(calcAvg(tips));
