498cc3a8ab20a2297580e0785342527516997041
eKorbi <bedriekorbi@gmail.com>
2026-02-20 20:09:34 +0100

    added more about destructuring

--- part9/script1.js
+++ part9/script1.js
@@ -8,6 +8,9 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+    }
 };
 
 
@@ -31,4 +34,28 @@ console.log(main,secondary)
 // main = secondary;
 // secondary = temp;
 
-[main,secondary] = [secondary,main]
+// [main, secondary] = [secondary, main];
+
+
+// receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+
+// nested destructuring
+const nested = [2, 4, [5, 6]];
+const [element1, , element3] = nested;
+console.log(element1,element3)
+
+//getting all elements
+const [i, j, [e, d]] = nested;
+console.log(i, j, e, d);
+
+
+// set default values for variables (when not knowing the length of array =e.g when getting data from an api)
+const [p, q, r] = [8, 9];
+console.log(p, q, r); //8,9 undefinded
+
+// setting default values
+const [u = 1, v = 1, z = 1] = [8, 9];
+console.log(u, v, z); //8,9,1
