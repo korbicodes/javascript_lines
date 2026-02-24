94a30f41c448fcace34744db63498085c24e83c1
eKorbi <bedriekorbi@gmail.com>
2026-02-24 12:06:56 +0100

    spread operator

---
+++ part9/script4.js
@@ -0,0 +1,73 @@
+'use strict';
+
+//spread operator: expand an array into all its elements.
+
+const arr = [7, 8, 9];
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, //open 24 hours
+            close: 24,
+        },
+        
+    },
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+    },
+    orderDelivery: function ({ starterIndex=1, mainIndex=0, time='22:00', address }) {
+        console.log(`${this.starterMenu[starterIndex]} and ${this.starterMenu[mainIndex]} will be delivered to ${address} at ${time}`)
+    },
+
+    
+};
+
+
+restaurant.orderDelivery({
+    time: '22:30',
+    address: 'london center 21',
+    mainIndex: 2,
+    starterIndex: 2,
+});
+
+//using the default values
+restaurant.orderDelivery({
+    address: 'london center 21',
+    starterIndex: 1,
+});
+
+
+const newMenu = [...restaurant.mainMenu, 'gnocci'];
+console.log(newMenu);
+
+// spread operator takes all elements from array and does not create new variables
+
+// shallow copy of arrays and merging arrays
+
+//copy
+const mainMenuCopy = [...restaurant.mainMenu];
+//join arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// spread op works with all iterables (arrays,strings,maps,sets. NOT objects)
+
+const str = 'jonas';
+const letters = [...str, '', 'S.'];
+console.log(letters);
+console.log(...str)
