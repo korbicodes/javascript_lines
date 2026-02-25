e399ee7d4b9129e8558179334cd813e7ed89c02b
eKorbi <bedriekorbi@gmail.com>
2026-02-25 05:06:36 +0100

    short circuiting AND operator

--- part9/script6.js
+++ part9/script6.js
@@ -18,8 +18,23 @@ console.log(undefined || 0 || '' || 'hello' || 23 || null); //hello
 const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
 console.log(guests1);
 
+
+//not good solution if first value is 0 as it is a falsy value and it will be ignored
 const guests2 = restaurant.numGuests || 10;  //10
 console.log(guests2);
 
 
+//AND
+// shorts circuits when first value is falsy
+console.log(0 && 'jonas'); //0
+// when values are truthy, eval continues and last value is returned. so its true only if all operands are true
+console.log(7 && 'jonas') //jonas
 
+console.log('hello' && 23 && null && 'jonas'); //null - first value that is false
+
+//practical eg
+if (restaurant.orderPizza) {
+    restaurant.orderPizza('mushrooms', 'spinach')
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
