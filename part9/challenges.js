b7e59fa1814987b471ffb96085ec3d7e401313f0
eKorbi <bedriekorbi@gmail.com>
2026-03-03 12:45:46 +0100

    finished more assignments

--- part9/challenges.js
+++ part9/challenges.js
@@ -311,7 +311,95 @@ for (let i = 0; i < books.length; i++) {
 
 // Some of the book objects from the books array are missing the edition property. Loop over the books array, and assign this property with a number 1 (if it doesn't already exist). Use logical assignment operators.
 
-for (let i = 0; i < books.length; i++){
-  books[i].edition ??= 1;
-  console.log(books[i].edition)
+for (let i = 0; i < books.length; i++) {
+  books[i].edition ||= 1;
 }
+
+// Some of the book objects from the books array have the highlighted property, which by default is set to true. Iterate over the books array, and if the thirdParty.goodreads.rating property is less than 4.2, reassign it with false.
+for (let i = 0; i < books.length; i++){
+  books[i].highlighted &&= !(books[i].thirdParty.goodreads.rating < 4.2)
+}
+
+
+
+//---------------
+//looping arrays - the for of loop
+// Use the for-of loop to loop over the books array and sum the pages of all books. Use the pageSum variable below, and the pages property of the book objects.
+
+let pageSum = 0;
+for (let book of books) {
+  pageSum += book.pages;
+}
+console.log(pageSum);
+
+// Below is the allAuthors variable which stores an empty array. Use the for-of loop to fill allAuthors with the authors of each book from the books array.
+
+// Remember that each book object has the author property, which can be a string (if there is only a single author) or an array (if there are multiple authors). You may need to use the typeof operator. You can also use multiple loops if needed. The allAuthors array should have just one level (no nested arrays).
+const allAuthors = [];
+
+// for (let book of books) {
+//   typeof(book.author) === "string" ? allAuthors.push(book.author) : allAuthors.push(...book.author)
+// }
+// console.log(allAuthors)
+
+//or 
+for (const book of books) {
+  if (typeof book.author === 'string') {
+    allAuthors.push(book.author)
+  } else {
+    for (const author of book.author) {
+      allAuthors.push(author);
+    }
+  }
+}
+console.log(allAuthors);
+
+
+// Use the for-of loop together with Array's entries() method to log each author from allAuthors to the console together with its index. Make the index start from 1, instead of 0.
+
+for (const [i, author] of allAuthors.entries()) {
+  console.log(`${i+1}. ${author}` )
+}
+
+//---------------
+//enhancd object literals
+// Using computed properties, fill the newBook object with the properties and values from the bookData array. The first one is done already.
+const bookData = [
+  ['title', 'Computer Networking: A Top-Down Approach'],
+  ['author', ['James F. Kurose', 'Keith W. Ross']],
+  ['publisher', 'Addison Wesley'],
+];
+
+
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  [bookData[1][0]]: bookData[1][1],
+  [bookData[2][0]]: bookData[2][1]
+};
+
+// Below is the pages variable. Add it as a property of the newBook2 object. Use the shorter way.
+
+
+const pages = 880;
+
+const newBook2 = {
+  title: 'The C Programming Language',
+  author: ['Brian W. Kernighan', 'Dennis M. Ritchie'],
+  pages
+};
+
+
+
+//--------------optional chaining
+// Write a function called getFirstKeyword that takes the book object as an argument. This function should return the first keyword from the book's keywords property (array) or undefined (if the keywords property doesn't exist). It shouldn't throw an error. Use optional chaining for that.
+
+
+// Optional chaining (?.) is a JavaScript operator that safely accesses properties or methods on a value that might be null or undefined, without throwing an error.Optional chaining only checks the value immediately before ?.
+
+function getFirstKeyword(book) {
+  return book.keywords?.[0];
+}
+
+
+
+console.log(getFirstKeyword(books[1]));
