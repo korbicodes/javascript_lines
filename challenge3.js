eaaf557c3d5fcaf111ea3df4d27db0f0e4f3ba9b
eKorbi <bedriekorbi@gmail.com>
2026-03-05 14:32:52 +0100

    finished challenge 3

---
+++ challenge3.js
@@ -0,0 +1,48 @@
+'use strict'
+
+// 1. Create an array 'events' of the different game events that happened (no duplicates)
+// 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+// 3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+// 4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+//       [FIRST HALF] 17: ⚽️ GOAL
+
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+//1
+const events = new Set([...gameEvents.values()])
+console.log(events);
+
+//2
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//3
+console.log(`An event happened on avarage every ${90/gameEvents.size} minutes`);
+
+//4
+
+for (const [minute, event] of gameEvents) {
+    if (minute <= 45) {
+        console.log(`[FIRST HALF] : ${minute}: ${event}`);
+    } else {
+        console.log(`[SECOND HALF] : ${minute}: ${event}`);
+    }
+}
+//OR
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
